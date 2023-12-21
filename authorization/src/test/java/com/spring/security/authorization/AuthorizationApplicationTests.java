2dc6f304f803f42b3f7723f9b848d821765ce95c
Suren Anthony Vithanage <surenanthonyvithanage@gmail.com>
2023-12-21 14:41:50 +0530

    initial

---
+++ authorization/src/test/java/com/spring/security/authorization/AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka/src/main/java/com/spring/security/eureka/config/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.spring.security.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
